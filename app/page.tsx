293f5c38b3db167fa9b0b8ac50432ee0acaf107a
MNAF1337 <66649631+MNAF1337@users.noreply.github.com>
2024-03-15 11:05:47 -0300

    add auth

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-    return (
-        <div>
-            <h1 className="h1-bold">Stack Overflow Clone</h1>
-            <h2 className="h2-bold">Stack Overflow Clone</h2>
-            <h3 className="h3-bold">Stack Overflow Clone</h3>
-        </div>
-    );
-}
