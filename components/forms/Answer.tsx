5bdea4905e86cb9d91a0c3e6f6264c874e65e9ff
MNAF1337 <66649631+MNAF1337@users.noreply.github.com>
2024-07-15 15:56:59 -0300

    add answer ai button

--- components/forms/Answer.tsx
+++ components/forms/Answer.tsx
@@ -27,6 +27,7 @@ interface Props {
 const Answer = ({ question, questionId, authorId }: Props) => {
     const pathname = usePathname();
     const [isSubmitting, setIsSubmitting] = useState(false);
+    const [isSubmittingAI, setIsSubmittingAI] = useState(false);
     const { mode } = useTheme();
     const editorRef = useRef(null);
     const form = useForm<z.infer<typeof AnswerSchema>>({
@@ -59,6 +60,29 @@ const Answer = ({ question, questionId, authorId }: Props) => {
         }
     };
 
+    const generateAIAnswer = async () => {
+        if (!authorId) return;
+
+        setIsSubmittingAI(true);
+        try {
+            const response = await fetch(
+                `${process.env.NEXT_PUBLIC_SERVER_URL}/api/chatgpt`,
+                {
+                    method: "POST",
+                    body: JSON.stringify({ question }),
+                }
+            );
+
+            const aiAnswer = await response.json();
+
+            alert(aiAnswer.reply);
+        } catch (error) {
+            console.log(error);
+        } finally {
+            setIsSubmittingAI(false);
+        }
+    };
+
     return (
         <div>
             <div className="flex flex-col justify-between gap-5 sm:flex-row sm:items-center sm:gap-2">
@@ -67,7 +91,7 @@ const Answer = ({ question, questionId, authorId }: Props) => {
                 </h4>
                 <Button
                     className="btn light-border-2 gap-1.5 rounded-md px-4 py-2.5 text-primary-500 shadow-none dark:text-primary-500"
-                    onClick={() => {}}
+                    onClick={generateAIAnswer}
                 >
                     <Image
                         src="/assets/icons/stars.svg"
@@ -76,7 +100,7 @@ const Answer = ({ question, questionId, authorId }: Props) => {
                         height={12}
                         className="object-contain"
                     />
-                    Generate an AI Answer
+                    Generate AI Answer
                 </Button>
             </div>
 
