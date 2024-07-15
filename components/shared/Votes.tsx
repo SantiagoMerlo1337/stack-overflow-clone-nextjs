0c135573f2fd7400be45b7c5deebec1e84e74e63
MNAF1337 <66649631+MNAF1337@users.noreply.github.com>
2024-07-15 17:30:45 -0300

    add toaster

--- components/shared/Votes.tsx
+++ components/shared/Votes.tsx
@@ -10,6 +10,7 @@ import { formatAndDivideNumber } from "@/lib/utils";
 import Image from "next/image";
 import { usePathname, useRouter } from "next/navigation";
 import { viewQuestion } from "@/lib/actions/interaction.action";
+import { toast } from "../ui/use-toast";
 
 interface Props {
     type: string;
@@ -37,7 +38,10 @@ const Votes = ({
 
     const handleSave = async () => {
         if (!userId) {
-            return;
+            return toast({
+                title: "Please log in",
+                description: "You must be logged in to perform this action",
+            });
         }
 
         await toggleSaveQuestion({
@@ -45,11 +49,19 @@ const Votes = ({
             questionId: JSON.parse(itemId),
             path: pathname,
         });
+
+        return toast({
+            title: `Question ${!hasSaved ? "Saved in" : "Removed from"} your collection`,
+            variant: !hasSaved ? "default" : "destructive",
+        });
     };
 
     const handleVote = async (action: string) => {
         if (!userId) {
-            return;
+            return toast({
+                title: "Please log in",
+                description: "You must be logged in to perform this action",
+            });
         }
 
         if (action === "upvote") {
@@ -71,8 +83,10 @@ const Votes = ({
                 });
             }
 
-            // TODO: show a toast
-            return;
+            return toast({
+                title: `Upvote ${!hasupVoted ? "Successful" : "Removed"}`,
+                variant: !hasupVoted ? "default" : "destructive",
+            });
         }
 
         if (action === "downvote") {
@@ -94,7 +108,10 @@ const Votes = ({
                 });
             }
 
-            // TODO: show a toast
+            return toast({
+                title: `Downvote  ${!hasupVoted ? "Successful" : "Removed"}`,
+                variant: !hasupVoted ? "default" : "destructive",
+            });
         }
     };
 
