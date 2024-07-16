922d87373d90d9e0c41c4b707ef6312c3cb6b49f
MNAF1337 <66649631+MNAF1337@users.noreply.github.com>
2024-07-16 19:13:57 -0300

    fix auth import

--- components/shared/LeftSidebar.tsx
+++ components/shared/LeftSidebar.tsx
@@ -5,11 +5,12 @@ import { usePathname } from "next/navigation";
 import Link from "next/link";
 import Image from "next/image";
 import { Button } from "../ui/button";
-import { SignedOut, useAuth } from "@clerk/nextjs";
+import { SignedOut } from "@clerk/nextjs";
+import { auth } from "@clerk/nextjs/server";
 
 const LeftSidebar = () => {
     const pathname = usePathname();
-    const { userId } = useAuth();
+    const { userId } = auth();
     return (
         <section className="background-light900_dark200 light-border custom-scrollbar sticky left-0 top-0 flex h-screen flex-col justify-between overflow-y-auto border-r p-6 pt-36 shadow-light-300 dark:shadow-none max-sm:hidden lg:w-[266px]">
             <div className="flex flex-1 flex-col gap-6">
