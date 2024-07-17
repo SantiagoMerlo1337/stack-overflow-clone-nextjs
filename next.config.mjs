aa68c5b5bf408e173a8dae231c874519c38d51ae
MNAF1337 <66649631+MNAF1337@users.noreply.github.com>
2024-07-17 14:38:36 -0300

    Revert "change next.config.mjs"
    This reverts commit 0e4dadc4fd654f10a99da9fef577d9fffb2f48a7.

--- next.config.mjs
+++ next.config.mjs
@@ -12,6 +12,9 @@ const nextConfig = {
             },
         ],
     },
+};
+// next.config.js
+module.exports = {
     webpack: (config, { isServer }) => {
         if (!isServer) {
             config.resolve.fallback = {
