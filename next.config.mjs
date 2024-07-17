0e4dadc4fd654f10a99da9fef577d9fffb2f48a7
MNAF1337 <66649631+MNAF1337@users.noreply.github.com>
2024-07-17 13:06:33 -0300

    change next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -12,9 +12,6 @@ const nextConfig = {
             },
         ],
     },
-};
-// next.config.js
-module.exports = {
     webpack: (config, { isServer }) => {
         if (!isServer) {
             config.resolve.fallback = {
