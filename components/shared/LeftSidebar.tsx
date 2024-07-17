533f478ca1458e583bc1b382fc21826f3e1dbd14
MNAF1337 <66649631+MNAF1337@users.noreply.github.com>
2024-07-17 14:39:16 -0300

    Revert "fix auth import"
    This reverts commit 922d87373d90d9e0c41c4b707ef6312c3cb6b49f.

--- components/shared/LeftSidebar.tsx
+++ components/shared/LeftSidebar.tsx
@@ -5,12 +5,11 @@ import { usePathname } from "next/navigation";
 import Link from "next/link";
 import Image from "next/image";
 import { Button } from "../ui/button";
-import { SignedOut } from "@clerk/nextjs";
-import { auth } from "@clerk/nextjs/server";
+import { SignedOut, useAuth } from "@clerk/nextjs";
 
 const LeftSidebar = () => {
     const pathname = usePathname();
-    const { userId } = auth();
+    const { userId } = useAuth();
     return (
         <section className="background-light900_dark200 light-border custom-scrollbar sticky left-0 top-0 flex h-screen flex-col justify-between overflow-y-auto border-r p-6 pt-36 shadow-light-300 dark:shadow-none max-sm:hidden lg:w-[266px]">
             <div className="flex flex-1 flex-col gap-6">
